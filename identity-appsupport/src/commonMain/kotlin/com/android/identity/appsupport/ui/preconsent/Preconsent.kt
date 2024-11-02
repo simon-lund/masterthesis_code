78be5330505c408a88f05c3921e755da850ab358
Simon Lund <simon97lund@gmail.com>
2024-11-02 15:32:15 +0100

    :sparkles: auto generate ids for preconsents

--- identity-appsupport/src/commonMain/kotlin/com/android/identity/appsupport/ui/preconsent/Preconsent.kt
+++ identity-appsupport/src/commonMain/kotlin/com/android/identity/appsupport/ui/preconsent/Preconsent.kt
@@ -3,17 +3,18 @@ package com.android.identity.appsupport.ui.preconsent
 import com.android.identity.appsupport.ui.consent.ConsentDocument
 import com.android.identity.appsupport.ui.consent.ConsentField
 import com.android.identity.appsupport.ui.consent.ConsentRelyingParty
+import com.android.identity.util.UUID
 
 /**
  * Details with the document that is being displayed in the pre-consent list.
  *
- * @property id an identifier for the preconsent document
+ * @property id an identifier for the preconsent document, automatically generated if not provided
  * @property document the document whose fields are shared with the relying party
  * @property relyingParty the relying party that will receive the shared fields
  * @property consentFields the fields that will be shared with the relying party
  */
 data class Preconsent(
-    val id: String,
+    val id: String = UUID.randomUUID().toString(),
     val document: ConsentDocument,
     val relyingParty: ConsentRelyingParty,
     val consentFields: List<ConsentField>,
